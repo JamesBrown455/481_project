78f9322c509f9926f1c04aed9471ec744bc35c0b
Unknown <2yankeefans2@gmail.com>
2018-04-03 16:40:14 -0400

    Removed unused imports

--- results.py
+++ results.py
@@ -7,14 +7,16 @@
 #
 
 # Imports
-from sklearn import svm
-from sklearn import preprocessing
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 
 # Calculate the Results
 def calc_results(results_list, true_list):
     results_matrix = confusion_matrix(results_list, true_list)
+    accuracy = accuracy_score(results_list, true_list)
     
-    print("Results")
+    print("Confusion Matrix")
+    print(results_matrix)
+    print()
+    print("Accuracy: " + str(accuracy))
