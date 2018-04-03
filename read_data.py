d9ca00f7c74ec4064d942f5580418b16da91a821
D Gosselin <dgosselin.p@gmail.com>
2018-04-03 16:39:53 -0400

    Added classification function

--- read_data.py
+++ read_data.py
@@ -4,21 +4,6 @@
 # Dylan Gosselin <gosselind1@southernct.edu>
 
 
-def processData(data):
-    TRUECLASSINDEX = 12
-    # 12 is the area burned
-    INDICIESTOIGNORE = [0, 1, 2, 3]
-    # 0 and 1 are positional coordinates
-    # 2 and 3 are month and day of the week (ex: mon) respectively
-
-    trueValue = data[TRUECLASSINDEX]
-    for index in sorted(INDICIESTOIGNORE, reverse=True):
-        del(data[index])
-    sample = list(map(float, data))
-
-    return sample, trueValue
-
-
 def readfile():
     LINESTOIGNORE = 1  # First line is data headers
     FILEPATH = "./forestfires.csv"
@@ -34,6 +19,43 @@ def readfile():
             trueValueList.append(trueValue)
 
     return sampleList, trueValueList
+
+
+def processData(data):
+    TRUECLASSINDEX = 12
+    # 12 is the area burned
+    INDICIESTOIGNORE = [0, 1, 2, 3]
+    # 0 and 1 are positional coordinates
+    # 2 and 3 are month and day of the week (ex: mon) respectively
+
+    trueValue = classifyTrueValue(float(data[TRUECLASSINDEX]))
+    for index in sorted(INDICIESTOIGNORE, reverse=True):
+        del(data[index])
+    sample = list(map(float, data))
+
+    return sample, trueValue
+
+def classifyTrueValue(trueInput):
+    NODAMAGE = 0
+    LIGHTDAMAGE = 1
+    MEDIUMDAMAGE = 2
+    LARGEDAMAGE = 3
+    SEVEREDAMAGE = 4
+
+    trueValue = None
+
+    if trueInput == 0:
+        trueValue = NODAMAGE
+    elif trueInput < 25:
+        trueValue = LIGHTDAMAGE
+    elif trueInput < 75:
+        trueValue = MEDIUMDAMAGE
+    elif trueInput < 150:
+        trueValue = LARGEDAMAGE
+    else:
+        trueValue = SEVEREDAMAGE
+
+    return trueValue
 
 
 def tester():
